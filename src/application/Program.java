4afe81480f5bbcb635ce7eae749ed94c1ed6e3d3
EwertonILima <ewertoninaciolima@gmail.com>
2021-05-03 17:07:31 -0300

    DeparmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,15 +6,15 @@ import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
-	
+
 	public static void main(String[] args) {
-		
+
 		Department obj = new Department(1, "Books");
-		
+
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
-		
+
 		System.out.println(seller);
-		
+
 	}
 
 }
